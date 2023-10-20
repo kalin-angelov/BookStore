0520c22321db103579f920ab96fa30d36137dcec
Kalizar <kalinangelov@abv.bg>
2023-10-20 18:11:22 +0300

    Update App.js

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,8 +1,20 @@
-import { Header } from './Header/Header';
+import { Routes, Route } from "react-router-dom";
+
+import { Header } from './components/Header/Header';
+import { Main } from "./components/Main/Main";
+import { Register } from './components/Register/Register';
+
 
 function App() {
   return (
-    <Header />
+    <div className="main">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+    
   );
 }
 
